7e44966e9eb1d242a58fbe8ba530135a65468e8a
ferre22mija <ferreteriaceraamicasmijares@gmail.com>
2023-01-24 19:14:00 -0500

    commit login

--- facturanube/src/components/login/login.js
+++ facturanube/src/components/login/login.js
@@ -1,30 +1,55 @@
 
-import { Box } from '@chakra-ui/react'
-import { Input, Stack, InputGroup, InputLeftElement, InputRightElement } from '@chakra-ui/react'
-import { PhoneIcon, CheckIcon } from '@chakra-ui/icons'
+import { Box, Center } from '@chakra-ui/react'
+import { Input, Stack, InputGroup, InputLeftElement, InputRightElement,Button } from '@chakra-ui/react'
+import { PhoneIcon, CheckIcon, UnlockIcon } from '@chakra-ui/icons'
+import { FaUserAlt } from 'react-icons/fa'
+
+import { HiUserCircle, HiKey } from 'react-icons/hi'
+import { Icon } from '@chakra-ui/react'
+
 function Login() {
     return (
-        <Box w='100%' p={4} color='black'>
-            primero
-            <Stack spacing={4}>
-                <InputGroup>
+        <Box w='100%' p={4} color='black' bg="#CBEDD5">
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <Stack spacing={4} bg="#62B6B7" w="50%" m="auto" p="15px" borderRadius={"15px"}>
+                <Center>
+                    <Box bg="#97DECE" w="170px" p="30px"
+                        borderRadius="110px" top="10" pos="absolute" >
+                        <Center>
+                            <Icon as={HiUserCircle} boxSize={95} />
+                        </Center>
+                    </Box>
+                </Center>
+
+                <br></br>
+                <br></br>
+                <InputGroup colorScheme="cyan" focusBorderColor="white" variant="filled">
                     <InputLeftElement
                         pointerEvents='none'
-                        children={<PhoneIcon color='gray.300' />}
+                        children={<Icon as={FaUserAlt} color='black' />}
                     />
-                    <Input type='tel' placeholder='Phone number' />
+                    <Input type='tel' placeholder='Usuario' _placeholder={{ color: 'inherit' }} />
                 </InputGroup>
 
-                <InputGroup>
+                <InputGroup colorScheme="cyan" focusBorderColor="white" variant="filled">
                     <InputLeftElement
                         pointerEvents='none'
                         color='gray.300'
                         fontSize='1.2em'
-                        children='$'
+                        children={<Icon as={HiKey} color='black' />}
+
                     />
-                    <Input placeholder='Enter amount' />
-                    <InputRightElement children={<CheckIcon color='green.500' />} />
+                    <Input placeholder='Contraseña' _placeholder={{ color: 'inherit' }} />
                 </InputGroup>
+
+                <Button colorScheme='teal' size='lg'>
+                    Button
+                </Button>
             </Stack>
         </Box>
     )
