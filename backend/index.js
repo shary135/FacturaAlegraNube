82bc5207b215879c87d1a22c436843e27b7984dc
ferre22mija <ferreteriaceraamicasmijares@gmail.com>
2023-02-22 16:53:12 -0500

    guardado de login con reiniciado de servidor

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,8 @@ import express from "express";
 import cors from "cors";
 import session from "express-session"
 import dotenv from "dotenv"
-//import db from "./config/Database.js"
+import db from "./config/Database.js"
+import SequelizeStore from "connect-session-sequelize";
 //#================
 import UserRoute from "./routes/UserRoute.js";
 import ProductRoute from  "./routes/ProductRoute.js";
@@ -11,12 +12,19 @@ import AuthRoute from "./routes/AuthRoutes.js";
 dotenv.config();
 
 const app = express();
+
+const sessionStore = SequelizeStore(session.Store);
+
+const store = new sessionStore({
+    db:db
+})
 /*(async()=>{
     await db.sync();
 })();*/
 app.use(session({
     secret:process.env.SESSION_SECRET,
     resave:false,
+    store:store,
     saveUninitialized:true,
     cookie:{
         secure:"auto",
@@ -31,6 +39,9 @@ app.use(express.json())
 app.use(UserRoute);
 app.use(ProductRoute);
 app.use(AuthRoute)
+
+//store.sync();
+
 app.listen(process.env.APP_PORT,()=>{
     console.log("server up and running... on PORT" +process.env.APP_PORT )
 })
